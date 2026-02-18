a09178657fcf586220ec1bd146472c4fd9b9e602
WonderKicks38b <165923223+WonderKicks38b@users.noreply.github.com>
2026-02-18 19:40:27 +0000

    Update check_stock.py

--- check_stock.py
+++ check_stock.py
@@ -1,1 +1,23 @@
+import os
+import requests
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+def tg_send(text: str) -> None:
+    response = requests.post(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        data={
+            "chat_id": CHAT_ID,
+            "text": text
+        },
+        timeout=20,
+    )
+    print("Telegram response:", response.text)
+
+def main():
+    tg_send("🧪 TEST ALERT — PokeWonder connection successful.")
+
+if __name__ == "__main__":
+    main()
 
